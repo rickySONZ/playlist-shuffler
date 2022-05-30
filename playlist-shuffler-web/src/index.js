3368805f70002f77e6f0323a37fd1cbb486a9b4c
Ryan Erricson <ryanerricson@gmail.com>
2022-05-30 12:23:52 -0400

    add typescript and redux store

--- playlist-shuffler-web/src/index.js
+++ playlist-shuffler-web/src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer.ts';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+ReactDOM.render(
   <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
